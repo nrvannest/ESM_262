15df861f45418a0ddb400c1a3849bfb41e9cb9f4
Nikole Vannest <50924274+nrvannest@users.noreply.github.com>
2022-03-06 16:33:22 -0800

    Created heat assignment, updated heat stress and uploaded tmax data.

--- R/heat_stress.R
+++ R/heat_stress.R
@@ -1,21 +1,45 @@
 #' Function to calculate the effect of heat stress in trees
-#' @param tree_age age class of tree, 1 = seedling, 2 = immature adult, 3 = adult
-#' @param ppt (mm) water deficit for current year
+#' @param date YYYY-MM-DD
+#' @param tree_age age class of tree, 1 = seedling, 2 = sapling, 3 = adult
+#' @param ppt (mm) total precipitation for the year
 #' @param max_temp (degrees F) maximum temperature recorded
-#' @param min_temp (degrees F) minimum temperature recorded
-#' @returns mortality of each tree as a binary list
-
-age <- c(1, 2, 1, 2, 3, 2, 1, 3, 3, 3, 2, 1, 1)
-temp <- c(90, 95, 100, 101, 89, 95, 110, 118, 120) 
+#' @returns text with the risk associated with each temperature and ppt for each age class
 
 # Equation
-growth <- function(tree_age, ppt, max_temp, min_temp = 30){
-  climate <- max_temp*(ppt + min_temp)
-  a <- matrix(data = NA, nrow = length(max_temp), ncol = length(tree_age))
-  for(i in 1:length(tree_age)){
-    a[, i] <- tree_age[i]*climate
+growth <- function(date, tree_age, ppt, max_temp){
+  print(paste("Date: ", date))
+  if(tree_age == 1){
+    print("Seedling, higher risk of mortality from heat related event")
+    if(max_temp > 30 && ppt < 500){
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes high mortality risk."))
+    }else
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes low mortality risk."))
   }
-  # Create output that says if > than certain # mortality event occurs.
+  if(tree_age == 2){
+    print("Sapling, medium risk of mortality from heat related event")
+    if(max_temp > 35 && ppt < 200){
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes high mortality risk."))
+    }else
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes low mortality risk."))
+  }
+  if(tree_age == 3){
+    print("Adult, low risk of mortality from heat related event")
+    if(max_temp > 38 && ppt < 100){
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes high mortality risk."))
+    }else
+      print(paste("Maximum temperature of:", max_temp,
+            "degrees C and annual precipitation of:", ppt, 
+            "mm causes low mortality risk."))
+  }
 }
-growth(age, 500, temp)
-
+  
